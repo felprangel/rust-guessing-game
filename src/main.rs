e85e42181e540b38e3a26caec0fef0e7d2ed6283
felprangel <feliperangel897@gmail.com>
2024-10-31 23:27:42 -0300

    adiciona print no loop

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("Please input your guess.");
-
     loop {
+        println!("Please input your guess.");
+
         let mut guess = String::new();
 
         io::stdin()
